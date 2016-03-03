789cf16ffc129b1e6a3ab9d11c340441852adb03
Andres Vega <gydoar@hotmail.com>
2016-03-03 18:48:38 -0500

    terminando la página , falta ajustar detalles

--- footer.php
+++ footer.php
@@ -1,4 +1,11 @@
 
+<footer>
+	<div class="container">
+		<div class="ten columns offset-by-one">
+			<p>Desarrollado por <img src="images/logo-celebrity.png" alt=""></p>
+		</div>	
+	</div>
+</footer>
 
 <script src="http://code.jquery.com/jquery-latest.min.js"></script>
 
--- index.php
+++ index.php
@@ -135,7 +135,287 @@
       </div>
     </section>
   </section>
- 
+
+  <!-- Temas -->
+
+  <section class="temas">
+    <div class="container">
+      <div class="ten columns offset-by-one">
+        <img class="twelve columns" src="images/bg-agenda.png" alt="">
+      </div>
+
+      <div class="twelve columns tema1">
+        <div class="tema-numero"><p>1</p></div>
+        <div class="tema-text"><p>TEMA UNO<br><span>Neuroventas, Neuro <br>Oratoria y Neuro Riqueza</span></p></div>
+
+        <div class="temas-informacion">
+          <p><strong>Docente:</strong></p>
+          <h3>Jürgen Klaric</h3>
+          <p><strong>Objetivo</strong></p>
+          <p>Aprender de forma científica a vender, hablar en público y preparar tu mente hacia la prosperidad; ya que no sólo vas a  tener más éxito económico, sino SER más feliz, realizado y líder.</p>
+          <br>
+          <p><strong>NEURO VENTAS (1 hora)</strong></p>
+          <p>- Qué y cómo analizar antes de hablar.</p>
+          <p>- Descubrir por qué la gente compra y cómo estimular esto.</p>
+          <p>- Modelo de venta efectiva.</p>
+          <p>- Pirámide para ganar los SI’s.</p>
+          <p>- ¿Cómo desarmar un cliente duro y difícil?</p>
+          <p>- Modelo de discurso efectivo de venta, servicios y productos.</p>
+
+          <br>
+          <p><strong>NEURO ORATORIA (1 hora)</strong></p>
+          <p>- Aprende qué es lo que hace que te pongan atención.</p>
+          <p>- ¿Cómo escribir una conferencia contundente? Story telling de conferencias - Cómo hablar, moverse y vestir para conectar de forma contundente con tu audiencia.</p>
+
+          <br>
+          <p><strong>NEURO RIQUEZA (1 hora)</strong></p>
+          <p>- Descubre si tú le tienes miedo al dinero y por qué no eres rico.</p>
+          <p>- Datos científicos e históricos de por qué los latinos tenemos la mente más pobre del mundo.</p>
+          <p>- Qué hacer para cambiar tu mentalidad, logrando riqueza y prosperidad.</p>
+
+          <br>
+          <p><strong>¿Qué te llevarás de forma garantizada?</strong></p>
+          <p>Aprender a vender, orar y descubrir si tienes mente de pobre o no. Según lo anterior, implementar un par de tips prácticos para lograr abundancia y prosperidad de forma adecuada.</p>
+        </div>  
+      </div>
+
+      <!-- Tema 2 -->
+      <div class="twelve columns tema1">
+        <div class="tema-numero"><p>2</p></div>
+        <div class="tema-text"><p>TEMA DOS<br><span>Neuroempoderamiento</span></p></div>
+
+        <div class="temas-informacion">
+          <p><strong>Docente:</strong></p>
+          <h3>Mauricio Bock</h3>
+          <p><strong>Objetivo</strong></p>
+          <p>Trabajar en equipo con tu mente te hará ser y lograr lo que quieras sin tanto desgaste. Aprende a crear las mejores condiciones y entornos para convertirte en un ser especial, el cual pueda operar y lograr liderazgo de forma sostenible.</p>
+          <br>
   
+          <p><strong>¿Qué te llevarás de forma garantizada?</strong></p>
+          <p>Cómo hacer que tu mente y SER estén en sintonía para lograr más y desgastarse menos.</p>
+        </div>  
+      </div>
+
+      <!-- Tema 3 -->
+      <div class="twelve columns tema1">
+        <div class="tema-numero"><p>3</p></div>
+        <div class="tema-text"><p>TEMA TRES<br><span>Neuroempoderamiento</span></p></div>
+
+        <div class="temas-informacion">
+          <p><strong>Docente:</strong></p>
+          <h3>Nora Sarmiento</h3>
+          <p><strong>Objetivo</strong></p>
+          <p>Aprender qué y cómo se estimula a que la gente diga SI o NO.</p>
+          <br>
+
+          <p>Aprenda por qué la mente dice una cosa y hace otra; conoce qué debes
+            de activar o estimular para que la mente reaccione a favor de tu discurso,
+            producto o servicio.</p>
+          <br>
+
+          <p><strong>Temas a tratar</strong></p>
+          <p>- ¿Cómo funciona la mente?</p>
+          <p>- ¿Cómo predecir un proceso de compra favorable?</p>
+          <p>- Bases de algoritmos para determinar los estímulos.</p>
+          <p>- Verdades y mentiras del neuromarketing.</p>
+          <p>- Marco ético.</p>
+          <p>- Recomendaciones prácticas para vender, aprender y entender en
+                el mundo de los negocios.</p>
+          <br>
+          <p><strong>¿Qué te llevarás de forma garantizada?</strong></p>
+          <p>Cómo hacer que tu mente y SER estén en sintonía para lograr más y desgastarse menos.</p>
+        </div>  
+      </div>
+    </div>
+  </section>
+
+  <!-- Logo Biia -->
+
+  <section class="logo-biia">
+    <div class="container center">
+      <img class="sello" src="images/sello.png" alt="">
+      
+      <div class="twelve columns margin-top-20px">
+        <img src="images/logo-certificado-biia.png" alt="">
+      </div>    
+    </div>
+  </section>
+ 
+ <!-- Seccion Cerfificados -->
+
+ <section class="certificados">
+   <div class="container center">
+     <img class="twelve columns" src="images/certificados.png" alt="">
+     
+     <p><strong>Tu participación en este programa certificado tiene un alto valor curricular debido a que serás acredito al un titulo otorgado por BiiA Lab USA una de las instituciones más autenticas  y vanguardistas de los Estados Unidos. Al cursar y completar correctamente tu examen el cual deberás tomarlo en línea en www.BiiALAb.org en un tiempo máximo de 10 días              después de terminar el programa recibirás el mismo vía digital.</strong></p>
+
+     <p>*Si adquieres la inscripción con memorias contarás con este valioso material de estudio.</p>
+   </div>
+   <div class="center">
+      <h5><a class="boton-yellow" href="">Deseo inscribirme y recibir memorias del evento</a></h5>
+   </div>
+ </section>
+
+ <!-- Locaciones -->
+
+ <section class="locaciones">
+  <div class="container center">
+    <h3>LOCACIONES</h3>
+    <div class="ten columns offset-by-one">
+      <img class="twelve columns" src="images/locaciones.png" alt="">
+      <p>*Esta imagen es referencial</p>
+    </div>
+
+  </div> 
+ </section>
+
+ <!-- Tickets -->
+
+ <section class="tickets">
+   <div class="container center">
+    <div>
+      <h3>TICKETS</h3>
+    </div>
+    <div class="row">
+      <!-- plan 1-->
+       <div class="three columns plan-ticket">
+         <div class="title-plata">
+           <h4>GENERAL</h4>
+         </div>
+         <img src="images/icon-plata.png" alt="">
+         <p class="color-purpura">Precio Preventa</p>
+         <h5 class="color-gray"><span>$</span>250,000.00<span>COP</span></h5>
+         <div class="line-precio-preventa"><hr></div>
+
+         <p class="color-purpura">Precio Regular</p>
+         <h5 class="color-purpura"><span>$</span>350,000.00<span>COP</span></h5>
+
+         <ul class="color-purpura">
+           <li>Memorias digitales</li>
+           <li>Cerficicado digital</li>
+         </ul>
+
+         <a class="boton-comprar-tickets" href="">COMPRAR TICKETS</a>
+       </div>
+
+       <!-- Plan 2 -->
+       <div class="three columns plan-ticket">
+         <div class="title-plata">
+           <h4>GENERAL PLUS</h4>
+         </div>
+         <img src="images/icon-plata.png" alt="">
+         <p class="color-purpura">Precio Preventa</p>
+         <h5 class="color-gray"><span>$</span>250,000.00<span>COP</span></h5>
+         <div class="line-precio-preventa"><hr></div>
+
+         <p class="color-purpura">Precio Regular</p>
+         <h5 class="color-purpura"><span>$</span>350,000.00<span>COP</span></h5>
+
+         <ul class="color-purpura">
+           <li>Memorias digitales</li>
+           <li>Cerficicado digital</li>
+         </ul>
+
+         <a class="boton-comprar-tickets" href="">COMPRAR TICKETS</a>
+       </div>
+
+       <!-- Plan 3 -->
+       <div class="three columns plan-ticket">
+         <div class="title-oro">
+           <h4>V.I.P</h4>
+         </div>
+         <img src="images/icon-dorado.png" alt="">
+         <p class="color-purpura">Precio Preventa</p>
+         <h5 class="color-dorado"><span>$</span>500,000.00<span>COP</span></h5>
+         <div class="line-precio-preventa"><hr></div>
+
+         <p class="color-purpura">Precio Regular</p>
+         <h5 class="color-purpura"><span>$</span>700,000.00<span>COP</span></h5>
+
+         <ul class="color-purpura">
+           <li>Memorias digitales</li>
+           <li>Cerficicado digital</li>
+           <li>Posibilidad de participar en preguntas para tu negocio</li>
+           <li>Audioterapia de Neuroriqueza con tecnología bineural, sin audífonos</li>
+           <li>Acceso archivo desde la nube</li>
+         </ul>
+
+         <a class="boton-comprar-tickets" href="">COMPRAR TICKETS</a>
+       </div>
+
+       <!-- Plan 4 -->
+       <div class="three columns plan-ticket">
+         <div class="title-oro">
+           <h4>V.I.P + CENA</h4>
+         </div>
+         <img src="images/icon-dorado.png" alt="">
+         <p class="color-purpura">Precio Preventa</p>
+         <h5 class="color-dorado"><span>$</span>250,000.00<span>COP</span></h5>
+         <div class="line-precio-preventa"><hr></div>
+
+         <p class="color-purpura">Precio Regular</p>
+         <h5 class="color-purpura"><span>$</span>350,000.00<span>COP</span></h5>
+
+         <ul class="color-purpura">
+           <li>Memorias en carpeta dura</li>
+           <li>Certificado impreso</li>
+           <li>Libro Estamos Ciegos Dedicado</li>
+           <li>Foto con Jürgen</li>
+           <li>Audioterapia de Neuroriqueza con tecnologia bineural, con audífonos</li>
+           <li>Cena consultoría de 2 horas privada</li>
+         </ul>
+
+         <a class="boton-comprar-tickets" href="">COMPRAR TICKETS</a>
+       </div>
+    </div>  
+   </div>
+
+   <!-- Necesitas hablar con nosotros -->
+   <div class="container">
+    <div class="eight columns offset-by-two center call-to-action">
+      <h4>¿NECESITAS <span>HABLAR</span> CON NOSOTROS?</h4>
+
+      <div class="icon-tel">
+        <img width="120px;" src="images/icon-tel.png" alt="">
+      </div>
+      <div class="text-tel">
+        <p>Llámanos en Colombia al <br><a href="tel:(1) 381 2860">(1) 381 2860</a></p>
+      </div>
+      <br><br><br>
+      <h4>MEDIOS DE PAGO</h4>
+      <p>Puedes pagar de las siguientes formas:</p>
+      <img class="twelve columns" src="images/medios-pago.png" alt="">
+      
+      <div class="text-consignacion">
+        <p><strong>Consignación bancaria</strong><br>Cuenta de ahorros Bancolombia 895-769727-85<br>a nombre de Jurgen Klaric<br>con cédula de extranjería N° 356490.<br>Enviar comprobante de la transacción con nombre y teléfono<br>al correo electrónico:<br><a href="mailto:info@jurguenklaric.com">info@jurguenklaric.com</a></p>
+      </div>
+      
+    </div>
+   </div>
+ </section>
+
+ <!-- Plaza Mayor -->
+
+ <section class="plaza-mayor">
+   <div class="container center">
+     <h4>PLAZA <span>MAYOR</span></h4>
+
+     <div class="row">
+       <div class="six columns">
+         <img class="twelve columns" src="images/plaza-mayor.jpg" alt="">
+       </div>
+       <div class="six columns">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.153400291423!2d-75.57834868552409!3d6.243505328087787!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e4429ab627545c1%3A0x23674fd4c47ec19c!2sPlaza+Mayor!5e0!3m2!1ses!2ses!4v1457047329898" width="100%" height="324" frameborder="0" style="border:0" allowfullscreen></iframe>
+       </div>
+
+       <div class="twelve columns center">
+          <h5><a class="boton-yellow" href="">Solicito que me llamen</a></h5>
+       </div>
+     </div>
+   </div>
+ </section>
+  
+ <!-- background fotos -->
+ <section class="fotos"></section>
 
 <?php include "footer.php"; ?>
